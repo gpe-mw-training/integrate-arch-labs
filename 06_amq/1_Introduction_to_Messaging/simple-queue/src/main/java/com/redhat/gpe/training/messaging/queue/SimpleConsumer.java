0d631c5fd73b75ea0bb8a9b08afc063ebc20e927
honghua chin <hchin@Docker-Swarm.local>
2016-12-28 16:36:13 +0800

    Updated solution lab assets using exercise lab assets of Integration Architecture Workshop

---
+++ 06_amq/1_Introduction_to_Messaging/simple-queue/src/main/java/com/redhat/gpe/training/messaging/queue/SimpleConsumer.java
@@ -0,0 +1,68 @@
+package com.redhat.gpe.training.messaging.queue;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.jms.*;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+
+public class SimpleConsumer {
+    private static final Logger LOG = LoggerFactory.getLogger(SimpleConsumer.class);
+
+    private static final Boolean NON_TRANSACTED = false;
+    private static final String CONNECTION_FACTORY_NAME = "myJmsFactory";
+    private static final String DESTINATION_NAME = "queue/simple";
+    private static final int MESSAGE_TIMEOUT_MILLISECONDS = 120000;
+
+    public static void main(String args[]) {
+        Connection connection = null;
+
+        try {
+            // JNDI lookup of JMS Connection Factory and JMS Destination
+            Context context = new InitialContext();
+            ConnectionFactory factory = (ConnectionFactory) context.lookup(CONNECTION_FACTORY_NAME);
+            Destination destination = (Destination) context.lookup(DESTINATION_NAME);
+
+            connection = factory.createConnection("sjayanti-redhat.com", "redhat");
+            connection.start();
+
+            Session session = connection.createSession(NON_TRANSACTED, Session.AUTO_ACKNOWLEDGE);
+            MessageConsumer consumer = session.createConsumer(destination);
+
+            LOG.info("Start consuming messages from " + destination.toString() + " with " + MESSAGE_TIMEOUT_MILLISECONDS + "ms timeout");
+
+            // Synchronous message consumer
+            int i = 1;
+            while (true) {
+                Message message = consumer.receive(MESSAGE_TIMEOUT_MILLISECONDS);
+                if (message != null) {
+                    if (message instanceof TextMessage) {
+                        String text = ((TextMessage) message).getText();
+                        LOG.info("Got " + (i++) + ". message: " + text);
+                    }
+                } else {
+                    break;
+                }
+            }
+
+            consumer.close();
+            session.close();
+        } catch (Throwable t) {
+            LOG.error("JMS Issue : ", t);
+        } finally {
+            // Cleanup code
+            // In general, you should always close producers, consumers,
+            // sessions, and connections in reverse order of creation.
+            // For this simple example, a JMS connection.close will
+            // clean up all other resources.
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (JMSException e) {
+                    LOG.error("JMS Issue : ",e);
+                }
+            }
+        }
+    }
+}
